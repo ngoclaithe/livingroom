b5216a6dae760ae7438a007d1d0229c6c68c3a73
laingoc99 <laingoc99@gmail.com>
2024-06-10 23:12:14 +0700

    update2

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,27 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 #include <SoftwareSerial.h>
 #include "OneButton.h"
 #include <DHT.h>
+#include <avr/wdt.h>
 
 #define DHTTYPE DHT11
 
 String local_add = "0xbb";
-String rev_add = "0xff";
 int nutnhan1 = 2;
 const int lightPin = 8;
 const int dhtPin = 3;
-
+const int pirPin = 9;
+const int buzzerPin = 13;
 bool lightState = false;
 float temperature = 0.0;
 float humidity = 0.0;
+int pirState = 0;
 
 OneButton button1(nutnhan1, true);
 SoftwareSerial zigbeeSerial(11, 10);
 DHT dht(dhtPin, DHTTYPE);
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 void changeLightState();
 void readSensor();
@@ -27,13 +32,19 @@ void setup() {
   Serial.begin(9600);
   zigbeeSerial.begin(9600);
   pinMode(lightPin, OUTPUT);
+  pinMode(buzzerPin, OUTPUT);
+  pinMode(pirPin, INPUT);
   dht.begin();
-
+  lcd.init();
+  lcd.backlight();
+  digitalWrite(buzzerPin, LOW);
   button1.attachClick(changeLightState);
   Serial.println("Sender is ready.");
+  wdt_enable(WDTO_2S);
 }
 
 void loop() {
+  wdt_reset();
   button1.tick();
   receiveCommand();
   delay(10);
@@ -61,12 +72,24 @@ void changeLightState() {
 void readSensor() {
   temperature = dht.readTemperature();
   humidity = dht.readHumidity();
+  pirState = digitalRead(pirPin);
+
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Temp: ");
+  lcd.print(temperature);
+  lcd.print("C");
+  lcd.setCursor(0, 1);
+  lcd.print("Humidity: ");
+  lcd.print(humidity);
+  lcd.print("%");
 }
 
 void sendStatus() {
   String message = String("Light: ") + (lightState ? "ON" : "OFF") +
                    ", Temp: " + String(temperature) + "C" +
                    ", Humidity: " + String(humidity) + "%" +
+                   ", PIR: " + String(pirState) +
                    " From Add: " + local_add;
   zigbeeSerial.println(message);
   Serial.println("Sent: " + message);
@@ -83,6 +106,10 @@ void receiveCommand() {
     } else if (command == "OFF") {
       lightState = false;
       digitalWrite(lightPin, LOW);
+    } else if (command == "BUZZON") {
+      digitalWrite(buzzerPin, HIGH);
+    } else if (command == "BUZZOFF") {
+      digitalWrite(buzzerPin, LOW);
     }
   }
 }
